d3ff70bffbd41c9574aaeb28ae7e3712e851d66f
Alexander Moreno Rodriguez <almoreno@eliteflower.com>
2022-03-19 22:11:43 -0500

    Agregadas funciones delete, truncate y decorador @affectedRows en las funciones que aplica modificacion de linea

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # UpAllExcelToSQL
 Subir archivos contenido de archivos de Excel creando la tabla si no existe
+
+# instalar paquetes con:
+$ py -m pip install -r requirements.txt 
+
+# actualizar pip:
+$ py -m pip install --upgrade pip
+
+# config.json
+
+{
+	"db_con": {
+		"server": ,
+		"db": ,
+		"user": ,
+		"password": 
+	},
+	"files": [
+		{
+			"dir": ,
+			"excel_file": ,
+			"sheet": ,
+			"schema": ,
+			"table": ,
+			"truncate": ,
+			"index": 
+		}
+	]
+}
--- import.py
+++ import.py
@@ -1,27 +1,37 @@
+from itertools import count
+from numpy import where
 import pandas as pd
 from pylib.file import extractDataFile
 
 from pylib.mod.utils import parameters, workDirectory
-from pylib.pySQL import insertDataToSql, stringConnect
-
-ROOT = workDirectory()
+from pylib.pySQL import deleteDataToSql, insertDataToSql, stringConnect, truncateTable
 
 ROOT = workDirectory()
 DB_CON, FILES = parameters()
 
 strCon = stringConnect(DB_CON)
 
-for file in FILES:
-    url = r'{}\{}\\'.format(ROOT, file['dir'])
-    print(url)
-    df = extractDataFile(
-        url=url,
-        file=file['excel_file'],
-        sheet=file['sheet']
-    )
-    insertDataToSql(
-        srtCon=strCon,
-        schema=file['schema'],
-        table=file['table'],
-        data=df
-    )
+
+def run():
+
+    for file in FILES:
+        url = r'{}\{}\\'.format(ROOT, file['dir'])
+        print(url)
+        df = extractDataFile(
+            url=url,
+            file=file['excel_file'],
+            sheet=file['sheet']
+        )
+
+        insertDataToSql(
+            strCon=strCon,
+            schema=file['schema'],
+            table=file['table'],
+            data=df,
+            truncate=file['truncate'],
+            index=file['index']
+        )
+
+
+if __name__ == '__main__':
+    run()
